7801fa4e709de970a57da69f6cf9d4f86085bb91
tommylay1902 <tommylay.c@gmail.com>
2023-08-29 09:23:50 -0700

    testing successful github action build

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -8,6 +8,6 @@ public class PingPongController {
     record PingPong(String result){}
     @GetMapping("/ping")
     public PingPong getPingPong(){
-        return new PingPong("pong");
+        return new PingPong("pong but no ping");
     }
 }
