7ab1a578e149ec6d3496743dd0056ec3fb1e9634
tommylay1902 <tommylay.c@gmail.com>
2023-08-29 19:50:08 -0700

    updated webhook url

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -8,6 +8,6 @@ public class PingPongController {
     record PingPong(String result){}
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("JOSIE 6");
+        return new PingPong("JOSIE 7");
     }
 }
