47e69a1b6cd9c76c6a26f29559cdc9cb483b33b6
tommylay1902 <tommylay.c@gmail.com>
2023-09-03 10:51:25 -0700

    updates to test deploy workflow

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -8,6 +8,6 @@ public class PingPongController {
     record PingPong(String result){}
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("JOSIE 12");
+        return new PingPong("JOSIE 14");
     }
 }
