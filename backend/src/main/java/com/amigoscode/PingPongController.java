9042856a12ffde39ae9f039005bb9c448a150539
tommylay1902 <tommylay.c@gmail.com>
2023-09-04 10:13:21 -0700

    update

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -8,6 +8,6 @@ public class PingPongController {
     record PingPong(String result){}
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("JOSIE 48");
+        return new PingPong("JOSIE 49");
     }
 }
