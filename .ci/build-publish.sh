bd07414f33cfd5df755ed87e3c8773329384a299
tommylay1902 <tommylay.c@gmail.com>
2023-09-12 13:09:19 -0700

    changed name length requirement

--- .ci/build-publish.sh
+++ .ci/build-publish.sh
@@ -11,6 +11,7 @@ docker buildx build \
     --platform=linux/amd64,linux/arm64 \
     -t "${USERNAME}/${REPO}:${TAG}" \
     -t "${USERNAME}/${REPO}:latest" \
+    -f DOCKERFILE \
     "${@:2}" \
     --push \
     "$1"
