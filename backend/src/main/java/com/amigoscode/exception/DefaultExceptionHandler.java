c23f7d510c54354bb10714dd14f0bd3fbc11ac72
tommylay1902 <tommylay.c@gmail.com>
2023-09-14 16:33:09 -0700

    fixed delete customer test

--- backend/src/main/java/com/amigoscode/exception/DefaultExceptionHandler.java
+++ backend/src/main/java/com/amigoscode/exception/DefaultExceptionHandler.java
@@ -4,21 +4,42 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.logging.Logger;
 
 @ControllerAdvice
 public class DefaultExceptionHandler {
+    private final Logger LOGGER = Logger.getLogger(DefaultExceptionHandler.class.getName());
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiError> handleException(ResourceNotFoundException e, HttpServletRequest request, HttpServletResponse response){
+
+           ApiError apiError = new ApiError(
+                    request.getRequestURI(),
+                    e.getMessage(),
+                    HttpStatus.NOT_FOUND.value(),
+                    LocalDateTime.now()
+            );
+
+        return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiError> handleException(UsernameNotFoundException e, HttpServletRequest request, HttpServletResponse response){
+
+        LOGGER.info("hit this exception handler");
+
         ApiError apiError = new ApiError(
-                request.getRequestURI(),
-                e.getMessage(),
-                HttpStatus.NOT_FOUND.value(),
-                LocalDateTime.now()
+            request.getRequestURI(),
+            e.getMessage(),
+            HttpStatus.NOT_FOUND.value(),
+            LocalDateTime.now()
         );
+
 
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
     }
--- backend/src/test/java/com/amigoscode/journey/CustomerIT.java
+++ backend/src/test/java/com/amigoscode/journey/CustomerIT.java
@@ -105,65 +105,84 @@ public class CustomerIT {
     }
     @Test
     void canDeleteCustomer() {
-
-        //CREATE A CUSTOMER
         Faker faker = new Faker();
         Name fakerName = faker.name();
 
         String name = fakerName.fullName();
-        String email = fakerName.firstName() + UUID.randomUUID() + "@gmail.com";
+        String email = fakerName.lastName() + "-" + UUID.randomUUID() + "@amigoscode.com";
+        int age = RANDOM.nextInt(1, 100);
 
-        int age = RANDOM.nextInt(1,100);
+        Customer.Gender gender = age % 2 == 0 ? Customer.Gender.MALE : Customer.Gender.FEMALE;
 
-        CustomerRegistrationRequest request = new CustomerRegistrationRequest(name, email, "password", age, Customer.Gender.MALE);
-        //send a post request
-        String jwtToken = webTestClient.post().uri(baseURI)
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(
+                name, email, "password", age, gender
+        );
+
+        CustomerRegistrationRequest request2 = new CustomerRegistrationRequest(
+                name, email + ".uk", "password", age, gender
+        );
+
+        // send a post request to create customer 1
+        webTestClient.post()
+                .uri(baseURI)
                 .accept(MediaType.APPLICATION_JSON)
                 .contentType(MediaType.APPLICATION_JSON)
-                .header(AUTHORIZATION)
-                .body(Mono.just(request), CustomerRegistrationRequest.class)                //.exchange is sending the request
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
                 .exchange()
                 .expectStatus()
-                .is2xxSuccessful()
+                .isOk();
+
+        // send a post request to create customer 2
+        String jwtToken = webTestClient.post()
+                .uri(baseURI)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request2), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus()
+                .isOk()
                 .returnResult(Void.class)
                 .getResponseHeaders()
                 .get(AUTHORIZATION)
                 .get(0);
 
-        //get all customers
-        List<CustomerDTO> customers = webTestClient.get()
+        // get all customers
+        List<CustomerDTO> allCustomers = webTestClient.get()
                 .uri(baseURI)
+                .accept(MediaType.APPLICATION_JSON)
                 .header(AUTHORIZATION, String.format("Bearer %s", jwtToken))
                 .exchange()
                 .expectStatus()
-                .is2xxSuccessful()
-                .expectBodyList(new ParameterizedTypeReference<CustomerDTO>() {})
+                .isOk()
+                .expectBodyList(new ParameterizedTypeReference<CustomerDTO>() {
+                })
                 .returnResult()
                 .getResponseBody();
 
-        //get customer by id
-        assert customers != null;
-        Long id = customers.stream()
-                .filter(c -> c.email().equals(email))
+
+        Long id = allCustomers.stream()
+                .filter(customer -> customer.email().equals(email))
                 .map(CustomerDTO::id)
                 .findFirst()
                 .orElseThrow();
 
-        //delete customer
+        // customer 2 deletes customer 1
         webTestClient.delete()
                 .uri(baseURI + "/{id}", id)
                 .header(AUTHORIZATION, String.format("Bearer %s", jwtToken))
+                .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus()
-                .is2xxSuccessful();
+                .isOk();
 
-        //get customers list after delete
+        // customer 2 gets customer 1 by id
         webTestClient.get()
                 .uri(baseURI + "/{id}", id)
+                .accept(MediaType.APPLICATION_JSON)
                 .header(AUTHORIZATION, String.format("Bearer %s", jwtToken))
                 .exchange()
                 .expectStatus()
-                .is4xxClientError();
+                .isNotFound();
     }
 
     @Test
