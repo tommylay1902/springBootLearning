cb2d32e5b9e28fc074ddcf5c015f1ff89c28e08c
tommylay1902 <tommylay.c@gmail.com>
2023-09-15 13:18:45 -0700

    update for loggin backend pings

--- backend/src/main/java/com/amigoscode/task/KeepRenderAliveSchedule.java
+++ backend/src/main/java/com/amigoscode/task/KeepRenderAliveSchedule.java
@@ -5,10 +5,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.logging.Logger;
+
 @Service
 public class KeepRenderAliveSchedule {
 
     private final RestTemplate restTemplate;
+    private final Logger LOGGER = Logger.getLogger(KeepRenderAliveSchedule.class.getName());
 
     @Value("${custom.api.base.uri.backend}")
     private String baseUriBackend;
@@ -21,6 +24,7 @@ public class KeepRenderAliveSchedule {
 
     @Scheduled(fixedRate = 780000L)
     public void pingBackendJob() {
+        LOGGER.info("pinging backend");
         String userJson = restTemplate.getForObject(baseUriBackend + "/ping", String.class);
 
     }
@@ -28,8 +32,8 @@ public class KeepRenderAliveSchedule {
 
     @Scheduled(fixedRate = 780000L)
     public void pingFrontendJob() {
+        LOGGER.info("pinging frontend");
         String userJson = restTemplate.getForObject(baseUriFrontend, String.class);
-
     }
 
 
